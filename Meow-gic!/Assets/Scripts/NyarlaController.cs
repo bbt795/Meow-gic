cbf5508fab8acbd82acd94dde6c87052ec91e4f7
BananaCat876 <jacq.custer@gmail.com>
2023-11-19 16:47:55 -0500

    Defined Player Controller

---
+++ Meow-gic!/Assets/Scripts/NyarlaController.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.Tilemaps;
+
+public class PlayerController : MonoBehaviour
+{
+    public GameObject myTarget;
+    public GameObject[] myTargets;
+    public Animator myAnim;
+    public SpriteRenderer myRenderer;
+    public Rigidbody2D myRig;
+    public float speed = 5.0f;
+    public Vector2 lastDirection;
+    // Start is called before the first frame update
+    public void onMove(InputAction.CallbackContext ev)
+    {
+        //Debug.Log("Inside Callback");
+        if (ev.performed)
+        {
+            lastDirection = ev.ReadValue<Vector2>();
+        }
+        if (ev.canceled)
+        {
+            lastDirection = Vector2.zero;
+            myAnim.SetInteger("DIR", 0);
+        }
+    }
+    void Start()
+    {
+        myAnim = this.GetComponent<Animator>();
+        myRenderer = this.GetComponent<SpriteRenderer>();
+        myRig = this.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W)||Input.GetKeyDown(KeyCode.A)||Input.GetKeyDown(KeyCode.S)||Input.GetKeyDown(KeyCode.D))
+        {
+
+            if (myRenderer != null)
+            {
+                myAnim.SetInteger("DIR", 1);
+
+            }
+
+        }
+        var velocity = new Vector2(lastDirection.x, lastDirection.y).normalized * speed;
+        myRig.velocity = new Vector2(velocity.x, myRig.velocity.y);
+    }
+}
