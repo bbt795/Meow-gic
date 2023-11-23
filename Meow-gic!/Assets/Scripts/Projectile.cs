1f950c99e4f9a5fc0effa9d212d7645526064e8a
BananaCat876 <jacq.custer@gmail.com>
2023-11-22 20:20:32 -0600

    Projectile Self-Deletion Applied

--- Meow-gic!/Assets/Scripts/Projectile.cs
+++ Meow-gic!/Assets/Scripts/Projectile.cs
@@ -11,7 +11,7 @@ public class Projectile : MonoBehaviour
         
     }
 
-    private void OnTriggerEnter2D(Collider2D other){
+    private void OnCollisionEnter2D(Collision2D other){
         Destroy(this.gameObject);
     }
 
