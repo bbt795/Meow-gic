e034bd1c0f367d47e66ad1df0b724c003fa957e9
BananaCat876 <jacq.custer@gmail.com>
2023-11-27 21:32:09 -0500

    Level Generator Offsets Defined

--- Meow-gic!/Assets/Scripts/LevelGenerator.cs
+++ Meow-gic!/Assets/Scripts/LevelGenerator.cs
@@ -6,41 +6,58 @@ using UnityEngine.InputSystem;
 public class LevelGenerator : MonoBehaviour
 {
     public GameObject[] levelPrefabs; // Array of prebuilt level structures
-    public int numberOfStructures = 10; // Number of structures to generate
-    public float structureSpacing = 30f; // Spacing between structures
-    // Start is called before the first frame update
+    public int numberOfStructures = 2; // Number of structures to generate
+    //public float structureSpacing = 30f; // Spacing between structures
+    public float horizontalSpacing = 30f;
+    public float verticalSpacing = 18f;
     public GameObject initialPrefab;
+    public int maxBranches = 3;
+    public int maxBranchLength = 5;
     public GameObject bossLevel;
     void Start()
     {
         initialPrefab = levelPrefabs[Random.Range(0, levelPrefabs.Length)];
         GenerateLevel();
+        //GenerateLevel(initialPrefab.transform.position, 0);
     }
 
     void GenerateLevel(){
+        Instantiate(initialPrefab, new Vector3(0f, 0f, 0f), Quaternion.identity, transform);
 
-        Instantiate(initialPrefab, new Vector2(0f, 0f), Quaternion.identity, transform);
+        GameObject prefabToInstantiate = levelPrefabs[Random.Range(0, levelPrefabs.Length)];
 
-        for (int i = 0; i < numberOfStructures; i++)
-        {
-            // Randomly select a prebuilt level structure from the array
-            GameObject prefabToInstantiate = levelPrefabs[Random.Range(0, levelPrefabs.Length)];
+        Vector2 position = initialPrefab.transform.position;
 
-            // Generate random position
-            Vector2 position = new Vector2(Random.Range(-30f, 30f), Random.Range(-30f, 30f));
+        //Offsets around each Level
+        //Vector2 offset = new Vector2(35f, 0f); //Right
+        //Vector2 offset = new Vector2(-35f, 0f); //Left
+        //Vector2 offset = new Vector2(0f, 23f); //Up
+        //Vector2 offset = new Vector2(0f, -23f); //Down
 
-            // Calculate random offset for placing structures around each other
-            Vector2 offset = new Vector2(Random.Range(-structureSpacing, structureSpacing), Random.Range(-structureSpacing, structureSpacing));
+        position += offset;
 
-            // Apply the offset to the position
-            position += offset;
+        GameObject obj = Instantiate(prefabToInstantiate, position, Quaternion.identity);
 
-            // Instantiate the prebuilt level structure at the calculated position
-            GameObject obj = Instantiate(prefabToInstantiate, position, Quaternion.identity);
+        // for (int i = 0; i < numberOfStructures; i++)
+        // {
+        //     // Randomly select a prebuilt level structure from the array
+        //     GameObject prefabToInstantiate = levelPrefabs[Random.Range(0, levelPrefabs.Length)];
 
-            // Optional: Parent the instantiated object to a container
-            obj.transform.parent = transform;
-        }
+        //     // Generate random position
+        //     Vector2 position = new Vector2(Random.Range(-30f, 30f), Random.Range(-30f, 30f));
+
+        //     // Calculate random offset for placing structures around each other
+        //     Vector2 offset = new Vector2(Random.Range(-structureSpacing, structureSpacing), Random.Range(-structureSpacing, structureSpacing));
+
+        //     // Apply the offset to the position
+        //     position += offset;
+
+        //     // Instantiate the prebuilt level structure at the calculated position
+        //     GameObject obj = Instantiate(prefabToInstantiate, position, Quaternion.identity);
+
+        //     // Optional: Parent the instantiated object to a container
+        //     obj.transform.parent = transform;
+        // }
     }
 
     // Update is called once per frame
