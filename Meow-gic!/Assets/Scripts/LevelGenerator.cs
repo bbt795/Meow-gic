8c574187e85c53ffcc3b0500e20304acb723a697
BananaCat876 <jacq.custer@gmail.com>
2023-11-27 23:27:51 -0500

    Level Generator Randomization Fix

--- Meow-gic!/Assets/Scripts/LevelGenerator.cs
+++ Meow-gic!/Assets/Scripts/LevelGenerator.cs
@@ -6,46 +6,35 @@ using UnityEngine.InputSystem;
 public class LevelGenerator : MonoBehaviour
 {
     public GameObject[] levelPrefabs; // Array of prebuilt level structures
-    public GameObject initialPrefab;
-    public int maxBranches = 3;
-    public int maxBranchLength = 2;
+    public int rows = 5; // Number of rows in the level
+    public int columns = 5; // Number of columns in the level
+    public float horizontalSpacing = 32f; // Horizontal spacing between prefabs
+    public float verticalSpacing = 23f; 
     public GameObject bossLevel;
     void Start()
     {
-        initialPrefab = levelPrefabs[Random.Range(0, levelPrefabs.Length)];
-        Instantiate(initialPrefab, new Vector3(0f, 0f, 0f), Quaternion.identity, transform);
-        GenerateLevel(initialPrefab.transform.position, 0);
+        GenerateLevel();
     }
 
-    void GenerateLevel(Vector2 position, int depth){
-
-        if (depth >= maxBranchLength)
+    void GenerateLevel(){
+        for (int row = 0; row < rows; row++)
         {
-            return;
-        }
+            for (int col = 0; col < columns; col++)
+            {
+                // Randomly choose a prefab from the array
+                GameObject prefab = levelPrefabs[Random.Range(0, levelPrefabs.Length)];
 
-        Vector2[] offsetPositions = new Vector2[]
-        {
-            position + new Vector2(35f, 0f), //Right
-            position + new Vector2(-35f, 0f), //Left
-            position + new Vector2(0f, 23f), //Up
-            position + new Vector2(0f, -23f) //Down
-        };   
+                // Calculate position based on row and column
+                float xPos = col * horizontalSpacing;
+                float yPos = row * verticalSpacing;
 
-        for (int i = 0; i < Mathf.Min(maxBranches, offsetPositions.Length); i++)
-        {
-            bool collisionDetected = Physics2D.OverlapCircle(offsetPositions[i], 0.1f);
-
-            if(!collisionDetected){
-                // Instantiate a random prefab variant at the predefined position
-                int randSet = Random.Range(0, 4);
-                GameObject randomPrefab = levelPrefabs[Random.Range(0, levelPrefabs.Length)];
-                GameObject newPrefab = Instantiate(randomPrefab, offsetPositions[i], Quaternion.identity);
-
-                // Recursively generate branches from the newly placed prefab
-                GenerateLevel(offsetPositions[i], depth + 1);
+                // Instantiate the prefab at the calculated position
+                GameObject obj = Instantiate(prefab, new Vector3(xPos, yPos, 0f), Quaternion.identity);
+                
+                // Make sure the instantiated object is in the same Z-plane as the Level Generator
+                obj.transform.SetParent(transform);
             }
-        }   
+        }
     }
 
     // Update is called once per frame
