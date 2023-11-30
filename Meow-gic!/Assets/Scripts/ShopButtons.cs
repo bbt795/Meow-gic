da1d541cfc3f29355c4f9c6669727b05864a4a3a
BananaCat876 <jacq.custer@gmail.com>
2023-11-30 03:10:54 -0500

    Added to Shop UI

--- Meow-gic!/Assets/Scripts/ShopButtons.cs
+++ Meow-gic!/Assets/Scripts/ShopButtons.cs
@@ -1,13 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
+using TMPro;
 
 public class ShopButtons : MonoBehaviour
 {
+    public GameObject gameManager;
+    // public GameObject healthPriceDisplay;
+    // public TextMeshProUGUI healthPriceDisplayText;
+    // public GameObject healthPriceDisplay;
+    // public TextMeshProUGUI healthPriceDisplayText;
+    public GameObject goldDisplay;
+    public TextMeshProUGUI goldDisplayText;
     // Start is called before the first frame update
     void Start()
     {
-        
+        gameManager = GameObject.FindGameObjectWithTag("GameManager");
+
     }
 
     // Update is called once per frame
--- Meow-gic!/Assets/Scripts/ShopKeepers.cs
+++ Meow-gic!/Assets/Scripts/ShopKeepers.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ShopKeepers : MonoBehaviour
+public class ShopKeepers : NPC
 {
     // Start is called before the first frame update
     void Start()
