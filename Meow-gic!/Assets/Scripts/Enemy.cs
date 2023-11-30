61414c205808eab1175b2b51e0b37f20d6ad9dfe
bbt795 <bubby1031@hotmail.com>
2023-11-30 15:32:38 -0500

    Added End Game Screen

--- Meow-gic!/Assets/Scripts/Enemy.cs
+++ Meow-gic!/Assets/Scripts/Enemy.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Enemy : Entity
 {
@@ -122,6 +123,14 @@ public class Enemy : Entity
             CreateCoins();
             CreatePotions();
             Destroy(this.gameObject);
+
+            if(gameObject.name == "Tree")
+            {
+
+                SceneManager.LoadScene(0);
+
+            }
+
         }
     }
 }
